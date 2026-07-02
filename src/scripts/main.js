458424add188eb903a0fd7c6ca07cb5f64539c0d
Skoulskiy <yaskevychilya@gmail.com>
2026-07-02 13:31:29 +0300

    fixes all bugs cannot fixe with document so added with wall.listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,32 @@
-'use strict';
-
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
+wall.addEventListener('click', (e) => {
+  const maxHeight = wall.clientHeight - spider.clientHeight;
+  const maxWidth = wall.clientWidth - spider.clientWidth;
 
-document.addEventListener('click', (e) => {
-  const rect = wall.getBoundingClientRect();
+  const params = wall.getBoundingClientRect();
 
-  const x = e.clientX - rect.left - wall.clientLeft;
-  const y = e.clientY - rect.top - wall.clientTop;
+  let left = e.clientX - params.left - wall.clientLeft - spider.offsetWidth / 2;
+  let topValue =
+    e.clientY - params.top - wall.clientTop - spider.offsetHeight / 2;
 
-  let l = x - spider.offsetWidth / 2;
-  let t = y - spider.offsetHeight / 2;
+  if (left < 0) {
+    left = 0;
+  }
 
-  l = clamp(l, 0, wall.clientWidth - spider.offsetWidth);
-  t = clamp(t, 0, wall.clientHeight - spider.offsetHeight);
+  if (topValue < 0) {
+    topValue = 0;
+  }
 
-  spider.style.left = `${l}px`;
-  spider.style.top = `${t}px`;
+  if (left > maxWidth) {
+    left = maxWidth;
+  }
+
+  if (topValue > maxHeight) {
+    topValue = maxHeight;
+  }
+
+  spider.style.top = `${topValue}px`;
+  spider.style.left = `${left}px`;
 });
